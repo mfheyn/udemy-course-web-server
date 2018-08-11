6ef61db5c48aced88923f00f181970071a367c46
Michael <ex@example.com>
2018-08-11 15:11:32 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+    // next();
+});
+
+//takes key-value pair
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    // res.send({
+    //     name: 'Michael',
+    //     likes: [
+    //         'Women',
+    //         'VR'
+    //     ]
+    // })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'This is the most awesome webserver in existence'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+// /bad route - send b ack json with errorMessage "Error handling request"
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Error handling request. Sorry! :('
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server has started!");
+});
